b9d76e11be8a6e4c258a46c0b5dace56a133682b
Crest <piotrek.ruzanski@gmail.com>
2025-03-26 15:32:35 +0100

    added style to contact form

--- src/app/components/GetInTouchSimple.tsx
+++ src/app/components/GetInTouchSimple.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { Button, Group, SimpleGrid, Textarea, TextInput, Title } from '@mantine/core';
-import { useForm, UseFormReturnType } from '@mantine/form';
+import { Button, Group, Textarea, TextInput, Title } from "@mantine/core";
+import { useForm, UseFormReturnType } from "@mantine/form";
 
 interface FormValues {
     name: string;
@@ -13,10 +13,10 @@ interface FormValues {
 export function GetInTouchSimple() {
     const form: UseFormReturnType<FormValues> = useForm({
         initialValues: {
-            name: '',
-            email: '',
-            subject: '',
-            message: '',
+            name: "",
+            email: "",
+            subject: "",
+            message: "",
         },
         validate: {
             name: (value) => value.trim().length < 2,
@@ -26,56 +26,72 @@ export function GetInTouchSimple() {
     });
 
     return (
-        <form onSubmit={form.onSubmit(() => {})}>
-            <Title
-                order={2}
-                size="h1"
-                style={{ fontFamily: 'Greycliff CF, var(--mantine-font-family)' }}
-                fw={900}
-                ta="center"
-            >
+        <form
+            onSubmit={form.onSubmit(() => {})}
+            className="max-w-lg mx-auto p-6 bg-neutral-900 rounded-2xl shadow-lg w-1/2 animate-fade-up"
+        >
+            <Title className="text-white text-2xl font-bold font-commissioner text-center mb-5">
                 Get in touch
             </Title>
-
-            <SimpleGrid cols={{ base: 1, sm: 2 }} mt="xl">
+            
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full font-commissioner">
                 <TextInput
                     label="Name"
-                    placeholder="Your name"
+                    placeholder=" Your name"
                     name="name"
                     variant="filled"
-                    {...form.getInputProps('name')}
+                    classNames={{
+                        input: "bg-[#525252] text-white placeholder-gray-400 w-full border  border-[#b2b2b2] rounded-sm focus:ring-2 focus:ring-blue-500",
+                        label: "text-white ",
+                    }}
+                    {...form.getInputProps("name")}
                 />
                 <TextInput
                     label="Email"
-                    placeholder="Your email"
+                    placeholder=" Your email"
                     name="email"
                     variant="filled"
-                    {...form.getInputProps('email')}
+                    classNames={{
+                        input: "bg-[#525252] text-white placeholder-gray-400 border w-full border-[#b2b2b2] rounded-sm focus:ring-2 focus:ring-blue-500",
+                        label: "text-white",
+                    }}
+                    {...form.getInputProps("email")}
                 />
-            </SimpleGrid>
+            </div>
 
-            <TextInput
-                label="Subject"
-                placeholder="Subject"
-                mt="md"
-                name="subject"
-                variant="filled"
-                {...form.getInputProps('subject')}
-            />
-            <Textarea
-                mt="md"
-                label="Message"
-                placeholder="Your message"
-                maxRows={10}
-                minRows={5}
-                autosize
-                name="message"
-                variant="filled"
-                {...form.getInputProps('message')}
-            />
+            <div className="mt-4 w-full font-commissioner">
+                <TextInput
+                    label="Subject"
+                    placeholder=" Subject"
+                    name="subject"
+                    variant="filled"
+                    classNames={{
+                        input: "bg-[#525252] text-white placeholder-gray-400 border border-[#b2b2b2] rounded-sm focus:ring-2 focus:ring-blue-500 w-full",
+                        label: "text-white",
+                    }}
+                    {...form.getInputProps("subject")}
+                />
+            </div>
 
-            <Group justify="center" mt="xl">
-                <Button type="submit" size="md">
+            <div className="mt-4 w-full font-commissioner">
+                <Textarea
+                    label="Message"
+                    placeholder=" Your message"
+                    maxRows={10}
+                    minRows={5}
+                    autosize
+                    name="message"
+                    variant="filled"
+                    classNames={{
+                        input: "bg-[#525252] w-full text-white placeholder-gray-400 border border-[#b2b2b2] rounded-sm focus:ring-2 focus:ring-blue-500",
+                        label: "text-white",
+                    }}
+                    {...form.getInputProps("message")}
+                />
+            </div>
+
+            <Group className="flex justify-center mt-6 font-commissioner">
+                <Button className="bg-neutral-600  text-white hover:bg-neutral-500 px-6 py-2 rounded-lg shadow-md">
                     Send message
                 </Button>
             </Group>
